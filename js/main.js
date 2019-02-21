f083ec628157fc1a534298827ca1bcdefc220f7d
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2019-02-21 14:46:02 -0300

    Se agrega identificación con localStorage

--- js/main.js
+++ js/main.js
@@ -150,4 +150,26 @@ $(document).ready(() => {
         }, 500);
     });
 
+    // Fake login
+    $('#login form').submit(() => {
+        var form_name = $('#form_name').val();
+
+        localStorage.setItem('form_name', form_name);
+    });
+
+    var form_name = localStorage.getItem('form_name');
+
+    if(form_name != null && form_name != 'undefined') {
+        var about_parrafo = $('#about p');
+
+        about_parrafo.html(`<br> <strong>Bienvenido: ${form_name}</strong>`);
+        about_parrafo.append('<br> <a href="#" id="logout">Cerrar sesión</a>');
+        $('#login').hide();
+
+        $('#logout').click(() => {
+            localStorage.clear();
+            location.reload();
+        });
+    }
+
 });
